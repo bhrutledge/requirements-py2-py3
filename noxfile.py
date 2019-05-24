bfb083e4adac5d069d70134213f17f8f339b5e33
Brian Rutledge <bhrutledge@gmail.com>
2019-05-24 16:13:00 -0400

    Add dev requirements

--- noxfile.py
+++ noxfile.py
@@ -4,10 +4,20 @@ import nox
 @nox.session(python=['2.7', '3.7'])
 def requirements(session):
     session.install('pip-tools')
-    session.run('pip-compile', '-o', f"requirements-{session.python}.txt")
+
+    session.run(
+        'pip-compile', '-o', f"requirements-{session.python}.txt",
+        'requirements.in',
+    )
+
+    session.run(
+        'pip-compile', '-o', f"requirements-{session.python}-dev.txt",
+         f"requirements-{session.python}.txt",
+         'requirements-dev.in',
+    )
 
 
 @nox.session(python=['2.7', '3.7'])
 def test(session):
-    session.install('-r', f"requirements-{session.python}.txt")
+    session.install('-r', f"requirements-{session.python}-dev.txt")
     session.run('pip', 'freeze')
