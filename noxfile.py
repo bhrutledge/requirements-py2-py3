90bb21b30cbefb84c4a3017166a767502b9f4ce1
Brian Rutledge <bhrutledge@gmail.com>
2019-05-28 10:18:42 -0400

    Move requirements to subdirectory

--- noxfile.py
+++ noxfile.py
@@ -4,20 +4,20 @@ import nox
 @nox.session(python=['2.7', '3.7'])
 def requirements(session):
     session.install('pip-tools')
+    session.chdir('requirements')
 
     session.run(
-        'pip-compile', '-o', f"requirements-{session.python}.txt",
-        'requirements.in',
+        'pip-compile', '-o', f"{session.python}-base.txt",
+        'base.in',
     )
 
     session.run(
-        'pip-compile', '-o', f"requirements-{session.python}-dev.txt",
-         f"requirements-{session.python}.txt",
-         'requirements-dev.in',
+        'pip-compile', '-o', f"{session.python}-dev.txt",
+         f"{session.python}-base.txt", 'dev.in',
     )
 
 
 @nox.session(python=['2.7', '3.7'])
 def test(session):
-    session.install('-r', f"requirements-{session.python}-dev.txt")
+    session.install('-r', f"requirements/{session.python}-dev.txt")
     session.run('pip', 'freeze')
