2586990cde935155da8cb048b8fb65ba95032b84
Brian Rutledge <bhrutledge@gmail.com>
2019-05-28 10:35:40 -0400

    Use tox default environment names

--- noxfile.py
+++ noxfile.py
@@ -1,23 +1,33 @@
 import nox
 
 
+def get_envname(python_version):
+    """Return the name a tox default environment"""
+    return f"py{python_version.replace('.', '')}"
+
+
 @nox.session(python=['2.7', '3.7'])
 def requirements(session):
+    envname = get_envname(session.python)
+
     session.install('pip-tools')
     session.chdir('requirements')
 
     session.run(
-        'pip-compile', '-o', f"{session.python}-base.txt",
+        'pip-compile',
+        '-o', f"base-{envname}.txt",
         'base.in',
     )
 
     session.run(
-        'pip-compile', '-o', f"{session.python}-dev.txt",
-         f"{session.python}-base.txt", 'dev.in',
+        'pip-compile',
+        '-o', f"dev-{envname}.txt",
+         f"base-{envname}.txt", 'dev.in',
     )
 
 
 @nox.session(python=['2.7', '3.7'])
 def test(session):
-    session.install('-r', f"requirements/{session.python}-dev.txt")
+    envname = get_envname(session.python)
+    session.install('-r', f"requirements/dev-{envname}.txt")
     session.run('pip', 'freeze')
